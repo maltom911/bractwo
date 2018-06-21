dadeff7bb40220bc3c8609c36cde702b55e16c8a
Tomek <maltom911@gmail.com>
2018-06-21 13:32:10 +0200

    reload

--- js/main.js
+++ js/main.js
@@ -10,10 +10,41 @@ $(document).ready(function () {
     });
 });
 
+/*$(document).ready(function () {
+    $('.header').hide();
+    $('.header').slideDown(3500);
+});*/
+
+function showImages(el) {
+    var windowHeight = jQuery(window).height();
+    $(el).each(function () {
+        var thisPos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos) {
+            $(this).addClass("fadeIn");
+        }
+    });
+}
+
+// if the image in the window of browser when the page is loaded, show that image
 $(document).ready(function () {
+    showImages('.history-photo');
+});
+
+// if the image in the window of browser when scrolling the page, show that image
+$(window).scroll(function () {
+    showImages('.history-photo');
+});
+
+$(document).ready(function () {
+    $('.welcome-caption').fadeIn(3500, function () {
+        $('.welcome-background').fadeOut(1500);
+    });
+});
+
+$(document).ready(function () {
+    $('.main-site-content').fadeIn(2500);
     $('.header').hide();
     $('.header').slideDown(3500);
 });
-
-
-
